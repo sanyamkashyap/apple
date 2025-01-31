3902c1a0bc7b6444ae3e90543114e8106d0d8465
sanyamkashyap <sanyamkashyap40@gmail.com>
2025-01-31 11:52:01 +0530

    creating hero section

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,9 +1,17 @@
-
+import gsap from "gsap";
+import { useGSAP } from "@gsap/react"
 
 const Hero = () => {
+    useGSAP(() => {
+        gsap.to("#hero", { opacity: 1, delay: 1.5 })
+    }, [])
+
     return (
-        <div>Hero</div>
-        
+        <section className="w-full nav-height bg-black relative">
+            <div className="h-5/6 w-full flex-center flex-col ">
+                <p id="hero" className="hero-title">iPhone 15 Pro</p>
+            </div>
+        </section>
     )
 }
 
